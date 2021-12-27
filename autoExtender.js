1edbede3bbeb7e0de56c782cc6a1903c3c2c5c3f
koji-genba <syon48690710@yahoo.co.jp>
2021-12-27 17:34:56 +0900

    [feat]学情激重防止のため時間延長回数に上限を設定(120分)

--- autoExtender.js
+++ autoExtender.js
@@ -1,8 +1,22 @@
-setInterval(autoExtender,60000) //10分ごとに起動
-//window.alert("autoExtender") //動作確認用
+//setInterval(autoExtender,60000) //10分ごとに起動
+////window.alert("autoExtender") //動作確認用
+//
+//function autoExtender(){
+//    if(document.getElementById("timeout-timer").textContent < 10){ //残り10分切ってたら
+//        document.getElementById("portaltimerimg").click(); //時計アイコンの画像をクリックする動作
+//    }
+//}
 
-function autoExtender(){
+
+
+var counter = 0;
+var timerId = setInterval(function(){
     if(document.getElementById("timeout-timer").textContent < 10){ //残り10分切ってたら
         document.getElementById("portaltimerimg").click(); //時計アイコンの画像をクリックする動作
     }
+    if(counter > 10){
+        clearInterval(timerId);
+    }
+    counter++;
 }
+, 60000)
