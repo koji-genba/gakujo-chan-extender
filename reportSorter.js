4e28cd03190985fbe5a8ecfac128ec072620aab8
koji-genba <syon48690710@yahoo.co.jp>
2021-12-27 13:03:41 +0900

    [feat] レポートソートに締め切り日時近い順にソートする機能を追加

--- reportSorter.js
+++ reportSorter.js
@@ -12,11 +12,11 @@ function main() {
         window.alert("reportSorter"); /*動作確認のwindow.alert(リリース時消す)*/
         /*レポートのテーブルはiframeの中らしいからまずiframeを取得*/
         elem = document.getElementById("main-frame-if");
-        window.alert(elem); /*動作確認のwindow.alert(リリース時消す)*/
+        //window.alert(elem); /*動作確認のwindow.alert(リリース時消す)*/
         /*iframeからレポートのテーブルを取得*/
         /*レポートのテーブルそのものにはidが無いから近くのidあるやつから2つ後ろってので表現してる*/
         table = elem.contentWindow.document.querySelector("#enqListForm table:nth-of-type(2)");
-        window.alert(table.innerHTML); /*動作確認のwindow.alert(リリース時消す)*/
+        //window.alert(table.innerHTML); /*動作確認のwindow.alert(リリース時消す)*/
 
         /*取得したやつをtextをコンソールに吐く，動作確認用*/
 
@@ -32,7 +32,7 @@ function main() {
             array[i]=[] /*配列を二次元にする，行内データを入れるため*/
             for(let j = 0; j < table.rows[0].cells.length; j++){ /*行内でのループ*/
                 array[i][j] = table.rows[i].cells[j].innerHTML; /*ボタンがすっ飛んだりするからHTMLで取る*/
-                /*ソートするときにやりやすいように提出状態に応じてフラグを立てる，フラグは配列の一番後ろ(ボタンのデータの次)に格納*/
+                /*ソートするときにやりやすいように提出状態に応じてフラグを立てる，フラグはボタンのデータの次に格納*/
                 if(j==2 && table.rows[i].cells[j].textContent.match('未提出')){
                     array[i][table.rows[0].cells.length+1] = 1;
                 }
@@ -42,11 +42,23 @@ function main() {
                 if(j==2 && table.rows[i].cells[j].textContent.match('提出済')){
                     array[i][table.rows[0].cells.length+1] = 3;
                 }
+                /*ソートするときにやりやすいように締め切り日時だけを切り出して，/と:を消して格納する，配列の一番後ろ(↑のフラグのデータの次)に格納*/
+                if(j==7){
+                    array[i][table.rows[0].cells.length+2] = table.rows[i].cells[j].textContent.substr(table.rows[i].cells[j].textContent.indexOf('～')+1);
+                    array[i][table.rows[0].cells.length+2] = array[i][table.rows[0].cells.length+2].replace("/","");
+                    array[i][table.rows[0].cells.length+2] = array[i][table.rows[0].cells.length+2].replace("/","");
+                    array[i][table.rows[0].cells.length+2] = array[i][table.rows[0].cells.length+2].replace(":","");
+                    array[i][table.rows[0].cells.length+2] = array[i][table.rows[0].cells.length+2].replace(" ","");
+                    console.log(array[i][table.rows[0].cells.length+2])
+                }
+            }
                 /*動作確認用*/
                 //console.log(array[i][j]);
                 //console.log(i+"+"+j);
-            }
         }
+
+        /*締め切り日時についてソート*/
+        array.sort(function(a,b){return(a[table.rows[0].cells.length+2] - b[table.rows[0].cells.length+2]);});
 
         /*提出状況についてソート*/
         array.sort(function(a,b){return(a[table.rows[0].cells.length+1] - b[table.rows[0].cells.length+1]);});
@@ -54,7 +66,7 @@ function main() {
         /*動作確認用*/
         //console.clear()
         //for(let i = 0; i < table.rows.length; i++){
-        //  console.log(array[i][2]);
+        //  console.log(array[i][7]);
         //}
 
         /*ソートしたデータでテーブルを書き換え*/
