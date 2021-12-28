92540bc0f05c8681080e499dd95e544698d80fb4
koji-genba <syon48690710@yahoo.co.jp>
2021-12-28 23:00:25 +0900

    [feat]時間延長を改めて修正，v0.32

--- autoExtender.js
+++ autoExtender.js
@@ -13,10 +13,10 @@ var counter = 0;
 var timerId = setInterval(function(){
     if(document.getElementById("timeout-timer").textContent <= 11){ //残り11分切ってたら
         document.getElementById("portaltimerimg").click(); //時計アイコンの画像をクリックする動作
+        counter++;
     }
     if(counter > 10){
         clearInterval(timerId);
     }
-    counter++;
 }
 , 60000)
