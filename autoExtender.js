24a19ece7ad457f6100103603e935cf576f6e403
koji-genba <syon48690710@yahoo.co.jp>
2021-12-28 18:12:12 +0900

    [fix]無限延長をもとの120分上限に修正

--- autoExtender.js
+++ autoExtender.js
@@ -1,22 +1,22 @@
-setInterval(autoExtender,60000) //10分ごとに起動
-//window.alert("autoExtender") //動作確認用
-
-function autoExtender(){
-    if(document.getElementById("timeout-timer").textContent < 10){ //残り10分切ってたら
-        document.getElementById("portaltimerimg").click(); //時計アイコンの画像をクリックする動作
-    }
-}
-
-
-
-//var counter = 0;
-//var timerId = setInterval(function(){
+//setInterval(autoExtender,60000) //10分ごとに起動
+////window.alert("autoExtender") //動作確認用
+//
+//function autoExtender(){
 //    if(document.getElementById("timeout-timer").textContent < 10){ //残り10分切ってたら
 //        document.getElementById("portaltimerimg").click(); //時計アイコンの画像をクリックする動作
 //    }
-//    if(counter > 10){
-//        clearInterval(timerId);
-//    }
-//    counter++;
 //}
-//, 60000)
+
+
+
+var counter = 0;
+var timerId = setInterval(function(){
+    if(document.getElementById("timeout-timer").textContent < 10){ //残り10分切ってたら
+        document.getElementById("portaltimerimg").click(); //時計アイコンの画像をクリックする動作
+    }
+    if(counter > 10){
+        clearInterval(timerId);
+    }
+    counter++;
+}
+, 60000)
