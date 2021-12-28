6ff6a0bc8ffffa3772e6e447ec3f584c74cf0900
koji-genba <syon48690710@yahoo.co.jp>
2021-12-28 22:25:08 +0900

    [fix]時間延長がたまに動かないため閾値を残り11分以下に変更

--- autoExtender.js
+++ autoExtender.js
@@ -11,7 +11,7 @@
 
 var counter = 0;
 var timerId = setInterval(function(){
-    if(document.getElementById("timeout-timer").textContent < 10){ //残り10分切ってたら
+    if(document.getElementById("timeout-timer").textContent <= 11){ //残り11分切ってたら
         document.getElementById("portaltimerimg").click(); //時計アイコンの画像をクリックする動作
     }
     if(counter > 10){
