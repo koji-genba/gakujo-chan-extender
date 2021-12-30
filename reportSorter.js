277e5d5748739ac54d5e80c22adc1ea594f55ed7
koji-genba <syon48690710@yahoo.co.jp>
2021-12-30 17:37:06 +0900

    [feat]レポート等提出画面において期限内→期限切の順で表示される機能を追加

--- reportSorter.js
+++ reportSorter.js
@@ -26,53 +26,55 @@ function main() {
         table = elem.contentWindow.document.querySelector("#enqListForm table:nth-of-type(2)");
 
 
-        var array = []; /*データ入れる配列，後で二次元にする*/
+        var array1 = []; /*データ入れる配列，後で二次元にする*/
 
         for(let i = 0; i < table.rows.length; i++){ /*行のループ*/
-            array[i]=[] /*配列を二次元にする，行内データを入れるため*/
+            array1[i]=[] /*配列を二次元にする，行内データを入れるため*/
             for(let j = 0; j < table.rows[0].cells.length; j++){ /*行内でのループ*/
-                array[i][j] = table.rows[i].cells[j].innerHTML; /*ボタンがすっ飛んだりするからHTMLで取る*/
+                array1[i][j] = table.rows[i].cells[j].innerHTML; /*ボタンがすっ飛んだりするからHTMLで取る*/
                 /*ソートするときにやりやすいように提出状態に応じてフラグを立てる，フラグはボタンのデータの次に格納*/
                 if(j==2 && (table.rows[i].cells[j].textContent.match('未提出') || table.rows[i].cells[j].textContent.match('Not submitted'))){
-                    array[i][table.rows[0].cells.length+1] = 1;
+                    array1[i][table.rows[0].cells.length+1] = 1;
                 }
                 if(j==2 && (table.rows[i].cells[j].textContent.match('一時保存') || table.rows[i].cells[j].textContent.match('Temporarily saved'))){
-                    array[i][table.rows[0].cells.length+1] = 2;
+                    array1[i][table.rows[0].cells.length+1] = 2;
                 }
                 if(j==2 && (table.rows[i].cells[j].textContent.match('提出済') || table.rows[i].cells[j].textContent.match('Submitted'))){
-                    array[i][table.rows[0].cells.length+1] = 3;
+                    array1[i][table.rows[0].cells.length+1] = 3;
                 }
                 /*ソートするときにやりやすいように締め切り日時だけを切り出して，/と:を消して格納する，配列の一番後ろ(↑のフラグのデータの次)に格納*/
                 if(j==7){
-                    array[i][table.rows[0].cells.length+2] = table.rows[i].cells[j].textContent.substr(table.rows[i].cells[j].textContent.indexOf('～')+1);
-                    array[i][table.rows[0].cells.length+2] = array[i][table.rows[0].cells.length+2].replace("/","");
-                    array[i][table.rows[0].cells.length+2] = array[i][table.rows[0].cells.length+2].replace("/","");
-                    array[i][table.rows[0].cells.length+2] = array[i][table.rows[0].cells.length+2].replace(":","");
-                    array[i][table.rows[0].cells.length+2] = array[i][table.rows[0].cells.length+2].replace(" ","");
+                    array1[i][table.rows[0].cells.length+2] = table.rows[i].cells[j].textContent.substr(table.rows[i].cells[j].textContent.indexOf('～')+1);
+                    array1[i][table.rows[0].cells.length+2] = array1[i][table.rows[0].cells.length+2].replace("/","");
+                    array1[i][table.rows[0].cells.length+2] = array1[i][table.rows[0].cells.length+2].replace("/","");
+                    array1[i][table.rows[0].cells.length+2] = array1[i][table.rows[0].cells.length+2].replace(":","");
+                    array1[i][table.rows[0].cells.length+2] = array1[i][table.rows[0].cells.length+2].replace(" ","");
                 }
             }
         }
 
         /*締め切り過ぎてるモノだけを別配列にコピー*/
-        var eols = [];
-        if(eol_notshow){
-            for(let i = 0; i < table.rows.length; i++){ /*行のループ*/
-                eols[i]=[]; /*配列を二次元にする，行内データを入れるため*/
-                if(array[i][table.rows[0].cells.length+2] < now){ //締め切り過ぎてたら
-                    for(let j = 0; j < table.rows[0].cells.length+2; j++){ /*行内でのループ*/
-                        eols[i][j] = array[i][j]; //データをコピー
-                        array[i][j] = null; //コピーしたら元配列ではnullに
+        var array2 = [];
+        if(1){
+            for(let i = 1; i < table.rows.length; i++){ /*行のループ*/
+                array2[i]=[]; /*配列を二次元にする，行内データを入れるため*/
+                if(array1[i][table.rows[0].cells.length+2] < now){ //締め切り過ぎてたら
+                    for(let j = 0; j <= table.rows[0].cells.length+2; j++){ /*行内でのループ*/
+                        array2[i][j] = array1[i][j]; //データをコピー
+                        array1[i][j] = null; //コピーしたら元配列ではnullに
                     }
                 }else{
-                    for(let j = 0; j < table.rows[0].cells.length+2; j++){ /*行内でのループ*/
-                        eols[i][j] = null;
+                    for(let j = 0; j <= table.rows[0].cells.length+2; j++){ /*行内でのループ*/
+                        array2[i][j] = null;
                     }
                 }
             }
         }
+
+
         //null要素を消して詰める
-        var active = array.filter(Boolean);
-        eols = eols.filter(v=>v);
+        var active = array1.filter(Boolean);
+        var eols = array2.filter(Boolean);
 
         /*締め切り日時についてソート*/
         active.sort(function(a,b){return(a[table.rows[0].cells.length+2] - b[table.rows[0].cells.length+2]);});
@@ -82,11 +84,34 @@ function main() {
         active.sort(function(a,b){return(a[table.rows[0].cells.length+1] - b[table.rows[0].cells.length+1]);});
         eols.sort(function(a,b){return(a[table.rows[0].cells.length+1] - b[table.rows[0].cells.length+1]);});
 
+        /*有効なレポートと期限切れレポートの配列をまとめる*/
+        var tasks = [];
+        var skip = 0;
+        for(let i=0; i<active.length; i++){
+            if(active[i][1]){
+                tasks[i-skip]=[];
+                for(let j = 0; j < table.rows[0].cells.length; j++){
+                    tasks[i-skip][j] = active[i][j];
+                }
+            }else{
+                skip++;
+            }
+        }
+        for(let i=active.length; i<(active.length + eols.length); i++){
+            if(eols[i-active.length][1]){
+                tasks[i-skip]=[];
+                for(let j = 0; j < table.rows[0].cells.length; j++){
+                    tasks[i-skip][j] = eols[i-active.length][j];
+                }
+            }else{
+                skip++;
+            }
+        }
+
         /*ソートしたデータでテーブルを書き換え*/
-        var filled = 0;
-        for(let i = 1; i < active.length; i++){
+        for(let i = 1; i < tasks.length; i++){
             for(let j = 0; j < table.rows[0].cells.length; j++){
-                table.rows[i].cells[j].innerHTML = active[i][j];
+                table.rows[i].cells[j].innerHTML = tasks[i][j];
                 /*一時保存の文字を青色に変える*/
                 if (table.rows[i].cells[j].textContent.match('一時保存')) {
                     table.rows[i].cells[j].innerHTML = "<font color=\"blue\">一時保存</font>";
@@ -95,7 +120,6 @@ function main() {
                     table.rows[i].cells[j].innerHTML = "<font color=\"blue\">Temporarily saved</font>";
                 }
             }
-            filled+=1;
         }
     }
 }
