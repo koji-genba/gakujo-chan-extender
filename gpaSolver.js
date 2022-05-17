46334eca16a12626cc57831c89d2b07ffe57adf2
DESKTOP-7509J76\syon4 <syon48690710@yahoo.co.jp>
2022-05-17 18:44:13 +0900

    [feat] 成績の画面でNo・開講番号・得点のそれぞれでのソート機能を追加

--- gpaSolver.js
+++ gpaSolver.js
@@ -19,6 +19,8 @@ function main() {
         table = elem.contentWindow.document.querySelector("#taniReferListForm+table");
         //window.alert(table.innerHTML); /*動作確認のwindow.alert(リリース時消す)*/
 
+
+
         var array1 = []
         var array2 = []
 
@@ -48,5 +50,113 @@ function main() {
         gpa = gp_sum / tan_sum;
 
         table.rows[0].cells[12].textContent = table.rows[0].cells[12].textContent + "\n GPA:" +gpa.toFixed(4);
+
+        //============================================================================================================
+        //ここまでGPA算出
+        //ここから開講番号順ソート
+        //============================================================================================================
+
+        nobutton = document.createElement("button");
+        nobutton.id = "nobutton";
+        nobutton.textContent = "No.でソート";
+        nobutton.addEventListener('click',function(){
+            sort_by_No(table);
+        });
+
+        opennumbutton = document.createElement("button");
+        opennumbutton.id = "opennumbutton";
+        opennumbutton.textContent = "開講番号でソート";
+        opennumbutton.addEventListener('click',function(){
+            sort_by_opennum(table);
+        });
+
+        scorebutton = document.createElement("button");
+        scorebutton.id = "scorebutton";
+        scorebutton.textContent = "得点でソート";
+        scorebutton.addEventListener('click',function(){
+            sort_by_score(table);
+        });
+
+        console.log("mainend");
+        document.getElementById("tabmenutable").appendChild(nobutton);
+        document.getElementById("tabmenutable").appendChild(opennumbutton);
+        document.getElementById("tabmenutable").appendChild(scorebutton);
+        console.log("mainendddd");
+
     }
+}
+
+function sort_by_No(){
+    elem = document.getElementById("main-frame-if");
+    table = elem.contentWindow.document.querySelector("#taniReferListForm+table");
+    var array1 = []
+
+    for(let i = 1; i < table.rows.length; i++){ /*行のループ*/
+        array1[i]=[] /*配列を二次元にする，行内データを入れるため*/
+        for(let j = 0; j < table.rows[0].cells.length; j++){ /*行内でのループ*/
+            array1[i][j] = table.rows[i].cells[j].textContent;
+        }
+    }
+    /*No.でソート*/
+    array1.sort((a,b)=>{
+        return Number(a[0]) - Number(b[0]);
+    });
+    /*ソートしたデータでテーブルを書き換え*/
+    for(let i = 0; i < array1.length; i++){
+        for(let j = 0; j < table.rows[0].cells.length; j++){
+            table.rows[i+1].cells[j].textContent = array1[i][j];
+        }
+    }
+    console.log(array1.length);
+}
+
+function sort_by_opennum(){
+    elem = document.getElementById("main-frame-if");
+    table = elem.contentWindow.document.querySelector("#taniReferListForm+table");
+    var array1 = []
+
+    for(let i = 1; i < table.rows.length; i++){ /*行のループ*/
+        array1[i]=[] /*配列を二次元にする，行内データを入れるため*/
+        for(let j = 0; j < table.rows[0].cells.length; j++){ /*行内でのループ*/
+            array1[i][j] = table.rows[i].cells[j].textContent;
+        }
+    }
+    /*開講番号でソート*/
+    array1.sort((a,b)=>{
+        if(a[3] < b[3]) return -1;
+        else if(a[3] > b[3]) return 1;
+        return 0;
+    });
+    /*ソートしたデータでテーブルを書き換え*/
+    for(let i = 0; i < array1.length; i++){
+        for(let j = 0; j < table.rows[0].cells.length; j++){
+            table.rows[i+1].cells[j].textContent = array1[i][j];
+        }
+    }
+    console.log(array1.length);
+}
+
+function sort_by_score(){
+    elem = document.getElementById("main-frame-if");
+    table = elem.contentWindow.document.querySelector("#taniReferListForm+table");
+    var array1 = []
+
+    for(let i = 1; i < table.rows.length; i++){ /*行のループ*/
+        array1[i]=[] /*配列を二次元にする，行内データを入れるため*/
+        for(let j = 0; j < table.rows[0].cells.length; j++){ /*行内でのループ*/
+            array1[i][j] = table.rows[i].cells[j].textContent;
+            console.log(array1[i][9]);
+        }
+    }
+    /*得点でソート*/
+    array1.sort((a,b)=>{
+        return Number(a[9]) - Number(b[9]);
+    });
+    /*ソートしたデータでテーブルを書き換え*/
+    for(let i = 0; i < array1.length; i++){
+        for(let j = 0; j < table.rows[0].cells.length; j++){
+            table.rows[i+1].cells[j].textContent = array1[i][j];
+        }
+    }
+    console.log(array1.length);
 }
