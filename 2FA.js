e110af13a4267f5b1221438c5fd047dd9998670c
koji-genba <95476617+koji-genba@users.noreply.github.com>
2023-03-02 02:32:51 +0900

    2fa動くようにした
    2fa動くようにした．
    秘密鍵はコード内ベタ打ち状態

--- 2FA.js
+++ 2FA.js
@@ -1,42 +1,54 @@
+
+//=====
+//2fa自動入力部分
 const DIGIT = 6;
 const TIME_STEP = 30;
 
-async function totp(secret_key){
-	const count = Math.floor(Date.now() / 1000 / TIME_STEP);
+function totp(key, date){
+    const counter = Math.floor(Date.now() / 1000 / TIME_STEP);
 
-    return await hotp(secret_key, count);
+    return hotp(key, counter);
 }
 
-async function hotp(secret_key, count){
+function hotp(key, counter){
+    const buf = Buffer.alloc(8);
+    buf.writeUInt32BE(Math.floor(counter/2**32), 0);
+    buf.writeUInt32BE(counter, 4);
 
-    
+    const val = trunc(hmacsha1(key, buf));
+    return val.toString().padStart(DIGIT, '0');
 }
 
+const crypto = require('crypto');
+function hmacsha1(key, message) {
+    const hmac = crypto.createHmac('sha1', key);
+    hmac.update(message);
+    return hmac.digest();
+}
+function trunc(data) {
+    const offset = data[data.length-1] & 0x0f;
+    const code = data.readUInt32BE(offset) & 0x7fffffff;
 
-//===========================================
+    return code % 10**DIGIT;
+}
 
 
 const base32_charlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
-
 function base32_decode(str){
-    //大文字にする
     str = str.toUpperCase(str)
-    //存在してはならない0と1を削除
     str = str.replace("0", '');
     str = str.replace("1", '');
-    //8の倍数長さになるように0(=A)パディング
     need = (Math.floor(str.length/8)+1) * 8 - str.length;
     str = str.padEnd(need+str.length, "A");
     console.log(str.length)
     console.log(need)
     console.log(str)
 
-    //リスト使って0-31にマッピングして5桁の二進数に変換
     var nums = ""
     for(let i = 0 ; i < str.length; i++){
         nums += base32_charlist.indexOf(str.charAt(i)).toString(2).padStart(5,"0")
     }
-    //8ビットずつ区切って10進数に変換
+
     var bit = new Uint8Array(nums.length/8)
     for(let i = 0; i < bit.length; i++){
         var bits = nums.substring(i*8, i*8+8);
@@ -47,22 +59,11 @@ function base32_decode(str){
 }
 
 
-
-
-//===========================================
-
-
-setTimeout(main(),1000);
-
-function main(){
-    console.log("main loaded");
+setTimeout(main,1500);
+function main() {
     document.getElementsByName("ninshoCode");
-    const key = "MED5OAG33SP766QP4PBSL3LQLVOLM4CE";
-    console.log(totp(key))
-    setInterval(()=>{
-        totp(key).then( code => {
-            document.getElementsByName("ninshoCode")[0].value = code;
-
-        })
-    },1000)
+    const key = base32_decode("KEY");
+    console.log(totp(key));
+    document.getElementsByName("ninshoCode")[0].value=totp(key);
 }
+
