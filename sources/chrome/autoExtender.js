5e358f979cc0d98c6665e25270ace6f7c340552a
koji-genba <95476617+koji-genba@users.noreply.github.com>
2023-06-07 22:34:19 +0900

    [update] 無限延長のコードどうせコメントアウトなので削除

--- sources/chrome/autoExtender.js
+++ sources/chrome/autoExtender.js
@@ -1,14 +1,3 @@
-//setInterval(autoExtender,60000) //10分ごとに起動
-////window.alert("autoExtender") //動作確認用
-//
-//function autoExtender(){
-//    if(document.getElementById("timeout-timer").textContent < 10){ //残り10分切ってたら
-//        document.getElementById("portaltimerimg").click(); //時計アイコンの画像をクリックする動作
-//    }
-//}
-
-
-
 var counter = 0;
 var timerId = setInterval(function(){
     if(document.getElementById("timeout-timer").textContent <= 11){ //残り11分切ってたら
