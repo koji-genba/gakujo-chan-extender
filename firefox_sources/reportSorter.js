c3b2cc0b1e682c27960687ffddc51682529bfd3c
koji-genba <95476617+koji-genba@users.noreply.github.com>
2023-12-16 17:33:31 +0900

    refactor: 多少マシになった

--- firefox_sources/reportSorter.js
+++ firefox_sources/reportSorter.js
@@ -1,15 +1,22 @@
 window.addEventListener("load", second, false);
 
-function second(){
-    setTimeout(main,1500); //1.5秒してから起動する
-}
 
 function main(){
+    /*レポートの表が表示されてから処理を開始するためのやつ*/
+    const Timer = setInterval(Loaded, 1000); //1秒まってから起動
+    function Loaded() {
+        if (document.getElementById("main-frame-if").contentWindow.document.querySelector("#taniReferListForm+table") != null) {
+            clearInterval(Timer);
+        }
+    }
 
-    //画面からレポート表取得して扱いやすい配列に変換
-    table = load();
-    array1 = makearray1(table);
+    setTempSaveButtonColorBlue()
+    MakeButton()
+    sort_by_date(LoadReportTable())
 
+}
+
+function MakeButton(){
     //ソート用ボタン生成1
     titlebutton = document.createElement("button");
     titlebutton.id = "titlebutton";
@@ -38,7 +45,10 @@ function main(){
     document.getElementById("tabmenutable").appendChild(titlebutton);
     document.getElementById("tabmenutable").appendChild(numberbutton);
     document.getElementById("tabmenutable").appendChild(datebutton);
+}
 
+function SetTempSaveButtonColorBlue(){
+    table = LoadReportTable()
     //一時保存の文字を青色に変える
     for(let i = 0; i < table.rows.length; i++){
         if (table.rows[i].cells[2].textContent.match('一時保存')) {
@@ -48,29 +58,22 @@ function main(){
             table.rows[i].cells[2].innerHTML = "<font color=\"blue\">Temporarily saved</font>";
         }
     }
-
-    //とりあえず提出期限でソート
-    sort_by_date(table);
-    document.getElementById("datebutton").click()
 }
 
-
-
-function load(){
+function LoadReportTable(){
     //引数:なし
     //返す:レポート等々の表
     //依存:なし
     //その他作用:なし
     var table;
     //レポートの表が表示されてから処理を開始するためのやつ
-    if (document.getElementById("main-frame-if") != null) {
-        elem = document.getElementById("main-frame-if");
-        table = elem.contentWindow.document.querySelector("#enqListForm table:nth-of-type(2)");
-    }
+    elem = document.getElementById("main-frame-if");
+    table = elem.contentWindow.document.querySelector("#enqListForm table:nth-of-type(2)");
+
     return table;
 }
 
-function makearray1(table){
+function makeReportArray(table){
     //引数:レポート等々の表
     //返す:↑を成形しデータ付加た二次元配列
     //依存:getdate(日付時刻取得), (load(レポート等々表取得))
@@ -79,52 +82,53 @@ function makearray1(table){
     //今の日付時刻取得
     now = getdate();
 
-    array1 = []; //データ入れる配列，後で二次元にする
+    ReportArray = []; //データ入れる配列，後で二次元にする
 
     for(let i = 1; i < table.rows.length; i++){ //行のループ
-        array1[i]=[] //配列を二次元にする，行内データを入れるため
+        ReportArray[i]=[] //配列を二次元にする，行内データを入れるため
         for(let j = 0; j < table.rows[0].cells.length; j++){ //行内でのループ
-            array1[i][j] = table.rows[i].cells[j].innerHTML; //提出ボタンがすっ飛んだりしないようにHTML生で取る
+            ReportArray[i][j] = table.rows[i].cells[j].innerHTML; //提出ボタンがすっ飛んだりしないようにHTML生で取る
             //ソートするときにやりやすいように提出状態に応じてフラグを立てる，フラグはボタンのデータの次に格納
             if(j==2 && (table.rows[i].cells[j].textContent.match('未提出') || table.rows[i].cells[j].textContent.match('Not submitted'))){
-                array1[i][table.rows[0].cells.length+1] = 1;
+                ReportArray[i][table.rows[0].cells.length+1] = 1;
             }
             if(j==2 && (table.rows[i].cells[j].textContent.match('一時保存') || table.rows[i].cells[j].textContent.match('Temporarily saved'))){
-                array1[i][table.rows[0].cells.length+1] = 2;
+                ReportArray[i][table.rows[0].cells.length+1] = 2;
             }
             if(j==2 && (table.rows[i].cells[j].textContent.match('提出済') || table.rows[i].cells[j].textContent.match('Submitted'))){
-                array1[i][table.rows[0].cells.length+1] = 3;
+                ReportArray[i][table.rows[0].cells.length+1] = 3;
             }
             //日付ソートするときにやりやすいように締め切り日時を切り出して，/と:を消して格納する，↑のフラグのデータの次に格納
             if(j==7){
-                array1[i][table.rows[0].cells.length+2] = table.rows[i].cells[j].textContent.substr(table.rows[i].cells[j].textContent.indexOf('～')+1);
-                array1[i][table.rows[0].cells.length+2] = array1[i][table.rows[0].cells.length+2].replace("/","");
-                array1[i][table.rows[0].cells.length+2] = array1[i][table.rows[0].cells.length+2].replace("/","");
-                array1[i][table.rows[0].cells.length+2] = array1[i][table.rows[0].cells.length+2].replace(":","");
-                array1[i][table.rows[0].cells.length+2] = array1[i][table.rows[0].cells.length+2].replace(" ","");
+                ReportArray[i][table.rows[0].cells.length+2] = table.rows[i].cells[j].textContent.substr(table.rows[i].cells[j].textContent.indexOf('～')+1);
+                ReportArray[i][table.rows[0].cells.length+2] = ReportArray[i][table.rows[0].cells.length+2].replace("/","");
+                ReportArray[i][table.rows[0].cells.length+2] = ReportArray[i][table.rows[0].cells.length+2].replace("/","");
+                ReportArray[i][table.rows[0].cells.length+2] = ReportArray[i][table.rows[0].cells.length+2].replace(":","");
+                ReportArray[i][table.rows[0].cells.length+2] = ReportArray[i][table.rows[0].cells.length+2].replace(" ","");
             }
         }
     }
-    return array1;
+    return ReportArray;
 }
 
-function sort_by_date(table){
-    //引数:レポート等々の表
+function sort_by_date(){
+    //引数:なし
     //返す:なし
-    //依存:makearray1(表->配列のため)
+    //依存:makeReportArray(表->配列のため)
     //その他作用:レポート等々の表を提出期限でソートする(未提出->一次提出->提出済)
 
     //テーブルを二次元配列にする
-    array1 = makearray1(table);
+    table = LoadReportTable()
+    ReportArray = makeReportArray(table);
 
     //締め切り過ぎてるモノだけを別配列にコピー
     var array2 = [];
-    for(let i = 1; i < array1.length; i++){ //行のループ
+    for(let i = 1; i < ReportArray.length; i++){ //行のループ
         array2[i]=[]; //配列を二次元にする，行内データを入れるため
-        if(array1[i][table.rows[0].cells.length+2] < now){ //締め切り過ぎてたら
+        if(ReportArray[i][table.rows[0].cells.length+2] < now){ //締め切り過ぎてたら
             for(let j = 0; j <= table.rows[0].cells.length+2; j++){ //行内でのループ
-                array2[i][j] = array1[i][j]; //データをコピー
-                array1[i][j] = null; //コピーしたら元配列ではnullに
+                array2[i][j] = ReportArray[i][j]; //データをコピー
+                ReportArray[i][j] = null; //コピーしたら元配列ではnullに
             }
         }else{
             for(let j = 0; j <= table.rows[0].cells.length+2; j++){ //行内でのループ
@@ -135,7 +139,7 @@ function sort_by_date(table){
 
 
     //null要素を消して詰める
-    var active = array1.filter(Boolean); //まだ期限になってないやつら
+    var active = ReportArray.filter(Boolean); //まだ期限になってないやつら
     var eols = array2.filter(Boolean); //期限過ぎてるやつら
     //締め切り日時についてソート
     active.sort(function(a,b){return(a[table.rows[0].cells.length+2] - b[table.rows[0].cells.length+2]);});
@@ -177,20 +181,21 @@ function sort_by_date(table){
     }
 }
 
-function sort_by_number(table){
-    //引数:レポート等々の表
+function sort_by_number(){
+    //引数:なし
     //返す:なし
-    //依存:makearray1(表->配列のため)
+    //依存:makeReportArray(表->配列のため)
     //その他作用:レポート等々の表を開講番号でソートする
 
     //テーブルを二次元配列にする
-    array1 = makearray1(table);
+    table = LoadReportTable()
+    ReportArray = makeReportArray(table);
 
     //締め切り日時についてソート
-    array1.sort(function(a,b){return(a[table.rows[0].cells.length+2] - b[table.rows[0].cells.length+2]);});
+    ReportArray.sort(function(a,b){return(a[table.rows[0].cells.length+2] - b[table.rows[0].cells.length+2]);});
 
     //開講番号でソート
-    array1.sort((a,b)=>{
+    ReportArray.sort((a,b)=>{
         if(a[3] < b[3]) return -1;
         else if(a[3] > b[3]) return 1;
         return 0;
@@ -198,27 +203,28 @@ function sort_by_number(table){
 
 
     //ソートしたデータでテーブルを書き換え
-    for(let i = 0; i < array1.length; i++){
+    for(let i = 0; i < ReportArray.length; i++){
         for(let j = 0; j < table.rows[0].cells.length; j++){
-            table.rows[i+1].cells[j].innerHTML = array1[i][j];
+            table.rows[i+1].cells[j].innerHTML = ReportArray[i][j];
         }
     }
 }
 
-function sort_by_title(table){
-    //引数:レポート等々の表
+function sort_by_title(){
+    //引数:なし
     //返す:なし
-    //依存:makearray1(表->配列のため)
+    //依存:makeReportArray(表->配列のため)
     //その他作用:レポート等々の表をタイトルでソートする
 
     //テーブルを二次元配列にする
-    array1 = makearray1(table);
+    table = LoadReportTable()
+    ReportArray = makeReportArray(table);
 
     //締め切り日時についてソート
-    array1.sort(function(a,b){return(a[table.rows[0].cells.length+2] - b[table.rows[0].cells.length+2]);});
+    ReportArray.sort(function(a,b){return(a[table.rows[0].cells.length+2] - b[table.rows[0].cells.length+2]);});
 
     //タイトルでソート
-    array1.sort((a,b)=>{
+    ReportArray.sort((a,b)=>{
         if(a[1] < b[1]) return -1;
         else if(a[1] > b[1]) return 1;
         return 0;
@@ -226,9 +232,9 @@ function sort_by_title(table){
 
 
     //ソートしたデータでテーブルを書き換え
-    for(let i = 0; i < array1.length; i++){
+    for(let i = 0; i < ReportArray.length; i++){
         for(let j = 0; j < table.rows[0].cells.length; j++){
-            table.rows[i+1].cells[j].innerHTML = array1[i][j];
+            table.rows[i+1].cells[j].innerHTML = ReportArray[i][j];
         }
     }
 }
