e619487b0043f1c9f6b491eea7c91d8b7610dd30
koji-genba <95476617+koji-genba@users.noreply.github.com>
2024-01-15 02:00:46 +0900

    feat: 既読にする個数を指定できるようにした

--- firefox_sources/message_reader.js
+++ firefox_sources/message_reader.js
@@ -13,6 +13,8 @@ function main() {
     }
     //既読用ボタン作成
     MakeButton()
+    //個数入力枠作成
+    MakeInputBox()
 }
 
 function LoadTable(){
@@ -32,13 +34,24 @@ function MakeButton(){
     //ソート用ボタン生成1
     var readbutton = document.createElement("button");
     readbutton.id = "readbutton";
-    readbutton.textContent = "No.でソート";
+    readbutton.textContent = "指定した個数を既読にする";
     readbutton.addEventListener('click',function(){
         Reader_call();
     });
 
     //ソート用ボタン追加
     document.getElementById("tabmenutable").appendChild(readbutton);
+}
+
+function MakeInputBox(){
+    var inputbox = document.createElement("input");
+    inputbox.id = "readNumInputBox"
+    inputbox.type = "number";
+    inputbox.defaultValue = "5";
+    inputbox.pattern = "\d*";
+    inputbox.oninput = "value = value.replace(/[^0-9]+/i,'');";
+    inputbox.placeholder = "既読にする数(半角数字)";
+    document.getElementById("tabmenutable").appendChild(inputbox);
 }
 
 function Reader_call(){
@@ -54,45 +67,26 @@ function Reader_call(){
             table_array[i][j] = table.rows[i].cells[j].innerHTML;
         }
     }
-    
-    var url = table_array[1][1]
-    url = url.substr(url.indexOf("=")+2)
-    url = url.substr(0, url.indexOf('">'))
-    while(url.indexOf('amp;')!=-1){
-        url = url.replace('amp;', '');
+
+    var InputBox = document.getElementById("readNumInputBox")
+    var ReadNum = InputBox.value
+    console.log(ReadNum)
+
+    for (let i = 1; i <= InputBox.value; i++) {
+        var url = table_array[i][1]
+        url = url.substr(url.indexOf("=")+2)
+        url = url.substr(0, url.indexOf('">'))
+        while(url.indexOf('amp;')!=-1){
+            url = url.replace('amp;', '');
+        }
+        console.log(url)
+        url = "https://gakujo.iess.niigata-u.ac.jp/campusweb/" + url
+
+        console.log(table_array[1][1])
+        console.log(url)
+
+
+        browser.runtime.sendMessage({url: url});
     }
-    console.log(url)
-    url = "https://gakujo.iess.niigata-u.ac.jp/campusweb/" + url
-
-    console.log(table_array[1][1])
-    console.log(url)
-
-
-    browser.runtime.sendMessage({url: url});
+    
 }
-
-//function sleep(waitSec, callbackFunc) {
-//
-//    var spanedSec = 0;
-//
-//    var waitFunc = function () {
-//
-//        spanedSec++;
-//
-//        if (spanedSec >= waitSec) {
-//            if (callbackFunc) callbackFunc();
-//            return;
-//        }
-//
-//        clearTimeout(id);
-//        id = setTimeout(waitFunc, 1000);
-//
-//    };
-//
-//    var id = setTimeout(waitFunc, 1000);
-//
-//  }
-//
-//  sleep(5, function() {
-//    console.log('5秒経過しました！');
-//  });
