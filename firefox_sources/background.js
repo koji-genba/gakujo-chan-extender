130c3c7e3c2e3ce1f7bf6fbf0e4ad587e8299749
koji-genba <95476617+koji-genba@users.noreply.github.com>
2024-01-15 01:15:59 +0900

    update: 自動で開いて勝手に閉じるように

--- firefox_sources/background.js
+++ firefox_sources/background.js
@@ -1,9 +1,10 @@
 browser.runtime.onMessage.addListener(function(message, sender, callback) {
     if ( message.url.indexOf('campusweb/')>0 ) {
-        browser.tabs.create({ url: message.url }).then(() => {
-            browser.tabs.executeScript({
-              code: `console.log('2');`,
+        browser.tabs.create({
+            'url': message.url ,
+            'active': false ,
+            },function(tab){
+                setTimeout(function(){chrome.tabs.remove(tab.id);}, 1000);
             });
-        });
     }
-});
+})
