fde9459b5bb67a9e6d874c645bf1b01825d5d4c8
koji-genba <95476617+koji-genba@users.noreply.github.com>
2024-01-27 14:51:44 +0900

    update: 各種命名変更+loadTable簡略

--- firefox_sources/message_reader.js
+++ firefox_sources/message_reader.js
@@ -1,84 +1,80 @@
 window.addEventListener("load", main, false);
 
 function main() {
-    /*通知一覧が表示されてから処理を開始するためのやつ*/
-    const Timer = setInterval(Loaded, 1000); //1秒まってから起動
+    //通知一覧が表示されてから処理を開始するためのやつ
+    const Timer = setInterval(Loaded, 1000); //1秒ごとに起動
     function Loaded() {
         if (document.getElementById("main-frame-if").contentWindow.document.querySelector("table.normal:nth-child(9)") != null) {
             clearInterval(Timer);
+            
+                //既読用ボタン作成
+                makeButton();
+                //個数入力枠作成
+                makeinputBox();
         }
     }
-    //既読用ボタン作成
-    MakeButton()
-    //個数入力枠作成
-    MakeInputBox()
 }
 
-function LoadTable(){
-    //通知の表をページから取得
-        //通知のテーブルはiframeの中らしいからまずiframeを取得
-    var elem = document.getElementById("main-frame-if");
-        //レポートのテーブルそのものにはidが無いから近くのidあるやつから1つ後ろってので表現してる
-    var table = elem.contentWindow.document.querySelector("table.normal:nth-child(9)");
+function loadTable(){
 
-    return table
+    return document.getElementById("main-frame-if").contentWindow.document.querySelector("table.normal:nth-child(9)");
 }
 
 
-function MakeButton(){
+function makeButton(){
     //既読用ボタン生成
-    var readbutton = document.createElement("button");
-    readbutton.id = "readbutton";
-    readbutton.textContent = "指定した個数を既読にする";
-    readbutton.addEventListener('click',function(){
-        Reader_call();
+    var readButton = document.createElement("button");
+    readButton.id = "readButton";
+    readButton.textContent = "指定した個数を既読にする";
+    readButton.addEventListener('click',function(){
+        readerCall();
     });
 
     //既読用ボタン追加
-    document.getElementById("tabmenutable").appendChild(readbutton);
+    document.getElementById("tabmenutable").appendChild(readButton);
 }
 
-function MakeInputBox(){
+function makeinputBox(){
     //既読個数設定用入力欄生成
-    var inputbox = document.createElement("input");
-    inputbox.id = "readNumInputBox"
-    inputbox.type = "number";
-    inputbox.defaultValue = "5";
-    inputbox.pattern = "\d*";
-    inputbox.oninput = "value = value.replace(/[^0-9]+/i,'');";
-    inputbox.placeholder = "既読にする数(半角数字)";
+    var inputBox = document.createElement("input");
+    inputBox.id = "readNumInputBox"
+    inputBox.type = "number";
+    inputBox.defaultValue = "5";
+    inputBox.pattern = "\d*";
+    inputBox.oninput = "value = value.replace(/[^0-9]+/i,'');";
+    inputBox.placeholder = "既読にする数(半角数字)";
 
     //既読個数設定用入力欄追加
-    document.getElementById("tabmenutable").appendChild(inputbox);
+    document.getElementById("tabmenutable").appendChild(inputBox);
 }
 
-function Reader_call(){
+function readerCall(){
 
     //表取得
-    var table = LoadTable()
-    var table_array = []
+    var table = loadTable()
+    var tableArray = []
 
     //表を二次元配列に
     for(let i = 1; i < table.rows.length; i++){ /*行のループ*/
-        table_array[i]=[] /*配列を二次元にする，行内データを入れるため*/
+        tableArray[i]=[] /*配列を二次元にする，行内データを入れるため*/
         for(let j = 0; j < table.rows[0].cells.length; j++){ /*行内でのループ*/
-            table_array[i][j] = table.rows[i].cells[j].innerHTML;
+            tableArray[i][j] = table.rows[i].cells[j].innerHTML;
         }
     }
 
     //個数入力欄から数取得
-    var InputBox = document.getElementById("readNumInputBox")
-    var ReadNum = InputBox.value
+    var inputBox = document.getElementById("readNumInputBox")
+    var readNum = inputBox.value
 
     //数じゃないのが入力されていた時の処理
-    if(isNaN(ReadNum)){
-        ReadNum = 0
+    if(isNaN(readNum)){
+        readNum = 0
     }
 
     //指定数分だけループ
-    for (let i = 1; i <= ReadNum; i++) {
+    for (let i = 1; i <= readNum; i++) {
         //リンクのとこのinnerHTML取得
-        var url = table_array[i][1]
+        var url = tableArray[i][1]
         //url切り出し&加工でリンク生成
         url = url.substr(url.indexOf("=")+2)
         url = url.substr(0, url.indexOf('">'))
