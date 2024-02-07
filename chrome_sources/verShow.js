94c3c0d514f837fae1df09afa29b49bc4986142f
koji-genba <95476617+koji-genba@users.noreply.github.com>
2024-02-07 20:10:13 +0900

    fix: chromeのためbrowserではなくchrome

--- chrome_sources/verShow.js
+++ chrome_sources/verShow.js
@@ -1,5 +1,5 @@
 const li = document.createElement("li");
 li.style.fontStyle="italic";
 li.style.fontSize="90%";
-li.appendChild(document.createTextNode("extension version " + browser.runtime.getManifest().version));
+li.appendChild(document.createTextNode("extension version " + chrome.runtime.getManifest().version));
 document.getElementById("tabmenu-ul").appendChild(li);
